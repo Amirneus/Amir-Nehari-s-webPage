c0689d69d586d5eb17db1ab9cefe9e6e7233fa16
Amirneus <AMIR.NEHARI@GMAIL.COM>
2025-07-21 12:14:40 +0300

    Update App.jsx
    add helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+import { Helmet } from "react-helmet"
 import './App.css'
 import Pages from "@/pages/index.jsx"
 import { Toaster } from "@/components/ui/toaster"
@@ -5,10 +7,17 @@ import { Toaster } from "@/components/ui/toaster"
 function App() {
   return (
     <>
+      <Helmet>
+        <title>Dr. Amir Nehari – Leadership Training & Organizational Psychology</title>
+        <meta name="description" content="Dr. Amir Nehari – Psychologist, Leadership Trainer, Group Facilitator based in Israel." />
+        <link rel="alternate" hreflang="he" href="https://www.amirnehari.com/Home-he" />
+      </Helmet>
+
+      {/* Actual UI Components */}
       <Pages />
       <Toaster />
     </>
-  )
+  );
 }
 
-export default App 
+export default App;
