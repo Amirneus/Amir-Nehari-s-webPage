ded5d96092645857de3518d3662021d60ff8dcc3
Amirneus <AMIR.NEHARI@GMAIL.COM>
2025-07-21 13:13:52 +0300

    Update main.jsx
    react helmet async

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx' // Keep App as an external component
-import '@/index.css'
+import App from './App.jsx'
+import { HelmetProvider } from 'react-helmet-async'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <HelmetProvider>
     <App />
-  </React.StrictMode>
+  </HelmetProvider>
 )
